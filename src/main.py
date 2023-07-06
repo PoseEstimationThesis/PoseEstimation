346d22d488f1752656568cbf2baf913f165e5a18
shmukli <wellplayedbb@gmail.com>
2023-07-06 22:52:55 +0200

    Basic interface preparation

--- src/main.py
+++ src/main.py
@@ -1,9 +1,13 @@
 import logic.Camera as Camera
+import visual.Interface as Intefrace
+
 
 
 def main():
    basic_object = Camera.CameraObject(0)
    basic_object.start_capturing()
+   basic_object_interface = Intefrace.BasicInterface()
+   basic_object_interface.initialize_interface()
 
 if __name__ == "__main__":
     main()
